ecbce49fcce10ea6eebb1fb6d66a68d25c546566
Calzio <162699126+Calzio@users.noreply.github.com>
2024-10-19 22:53:45 +0000

    teste de engenheiro

--- projeto/classes/engenheiro.py
+++ projeto/classes/engenheiro.py
@@ -1,33 +1,36 @@
-from classes.abstracts.pessoa import Pessoa
-from classes.endereco import Endereco
+from projeto.classes.abstracts.pessoa import Pessoa
+from projeto.classes.endereco import Endereco
 
 class Engenheiro(Pessoa):
     # Construtores
-    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco, crea: str) -> None:
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco, crea: str, salario_final: str) -> None:
         super().__init__(nome, telefone, email, endereco)
 
         self.crea = self._verificar_crea(crea)
-
+        self.salario_final_base_engenheiro = salario_final
     # Método abstrato
     def _verificar_crea(self,valor):
         """Método para verificar crea"""
 
         self._verificar_crea_tipo_invalido(valor)
+        self._verificar_crea_vazio(valor)
 
         self.crea = valor
         return self.crea
     
+    def inicializar(self):
+        return super().inicializar()
+      
     def _verificar_crea_tipo_invalido(self,valor):
         """Método para verificar crea inválido"""
         if not isinstance(valor, str):
             raise TypeError("O crea deve se manter como tipo texto!")
 
-    def salario_final(self) -> float:
-        return self.salario * 1.10
-    
-    def inicializar(self):
-        return super().inicializar()
+    def _verificar_crea_vazio(self,valor):
+        if not valor.strip():
+            raise ValueError("O CREA não pode estar vazio.")
 
-    # Implementando o método abstrato.
     def salario_final(self) -> float:
-        return self.salario_base  
+        return self.salario_final_base_engenheiro * 1.10
+    
+    
--- projeto/tests/test_engenheiro.py
+++ projeto/tests/test_engenheiro.py
@@ -1,19 +1,27 @@
-# import pytest
-# from projeto.tests.test_endereco import endereco_valido
-# from projeto.classes.engenheiro import Engenheiro
+import pytest
+from projeto.classes.engenheiro import Engenheiro
+from projeto.tests.test_endereco import endereco_valido
 
-# @pytest.fixture
-# def pessoa_valida():
-#     engenheiro = Engenheiro("Caio", "1234-5678", "Caioreal@gmail.com", endereco_valido, "2345")
-#     return engenheiro
+@pytest.fixture
+def engenheiro_valido():
+    engenheiro = Engenheiro("Caio", "1234-5678", "Caioreal@gmail.com", endereco_valido, "2345", "3000")
+    return engenheiro
 
-# def test_crea_valido(engenheiro_valido):
-#     assert engenheiro_valido.crea == "2345"
+def test_crea_valido(engenheiro_valido):
+    assert engenheiro_valido.crea == "2345"
 
-# def test_mudar_crea_valido(engenheiro_valido):
-#     engenheiro_valido.crea = "5135"
-#     assert engenheiro_valido.crea == "5135"
+def test_mudar_crea_valido(engenheiro_valido):
+    engenheiro_valido.crea = "5135"
+    assert engenheiro_valido.crea == "5135"
 
-# def test_crea_tipo_invalido():
-#     with pytest.raises(TypeError, match="O crea deve se manter como tipo texto!"):
-#         Engenheiro("Caio", "1234-5678", "Caioreal@gmail.com", endereco_valido, True)
+def test_mudar_salario_final(engenheiro_valido):
+    engenheiro_valido.salario_final = "6000"
+    assert engenheiro_valido.salario_final == "6000"
+
+def test_crea_tipo_invalido():
+    with pytest.raises(TypeError, match="O crea deve se manter como tipo texto!"):
+        Engenheiro("Caio", "1234-5678", "Caioreal@gmail.com", endereco_valido, True, "3000")
+
+def test_crea_vazio():
+    with pytest.raises(ValueError, match= "O CREA não pode estar vazio."):
+        Engenheiro("Caio", "1234-5678", "Caioreal@gmail.com", endereco_valido, "", "3000")
